c0877b001971bcd7e1fb9220b976b7e83f81ca2c
avimehenwal <avi.mehanwal@gmail.com>
2016-06-29 21:15:07 +0530

    go variable input

--- hello-world.go
+++ hello-world.go
@@ -1,13 +1,25 @@
 package main
 import "fmt"
 
-// this is a comment
+/*
+   GoLang Variables
+   - Simple Variable
+   - Default Variable
+   - Multiple Variables
+*/
+
 func main() {
-    //var x string             // go variables <var name> <type>
+    //var x string           // go variables <var name> <type>
     x := "Hello World"       // Var default values assignment. Skip type
     fmt.Println(x)
     x = "first "             // var x takes/assigned value of 'first'
     fmt.Println(x)
     x = x + "second"
     fmt.Println(x)
+
+    const (
+      a  = 5
+      pi = 3.14
+    )
+    fmt.Println(a, pi)
 }
--- strings.go
+++ strings.go
@@ -7,4 +7,10 @@ func main() {
     fmt.Println(len("Hello World"))  // length
     fmt.Println("Hello World"[1])    // str index byte representation
     fmt.Println("Hello " + "World")  // concatinate
+
+    fmt.Print("Enter a number: ")
+    var input float64
+    fmt.Scanf("%f", &input)
+    output := input * 2
+    fmt.Println(output)
 }
