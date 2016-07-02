8f80aae62452a7f946125c44023675b48a1762c8
avimehenwal <avi.mehanwal@gmail.com>
2016-07-02 12:39:00 +0530

    Goroutines and channels

---
+++ concurrency.go
@@ -0,0 +1,47 @@
+package main
+
+/*  Goroutines and Channels  */
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+	}
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+}
